3d4fb571443fdeb4ccfd4df0eb34b594e06197c1
iELectro13 <ejose.acc@gmail.com>
2021-12-03 19:30:16 -0500

    Static and templates structure defined, change in settings.py in order to define a new folder structure. Created authentication/login, register; and /create-todo

--- djangotodo/settings.py
+++ djangotodo/settings.py
@@ -57,7 +57,7 @@ ROOT_URLCONF = 'djangotodo.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -126,3 +126,4 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'djangotodo/static')]
--- todo/urls.py
+++ todo/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns =[
-    path('', views.index, name='Home')
+    path('', views.index, name='Home'),
+    path('create-todo', views.create_todo, name='create-todo')
 ]
